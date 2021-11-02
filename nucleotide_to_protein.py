f942162fb75d7dd06b686c4d326aaae2401be1eb
kmcooper <kmcooper@unomaha.edu>
2021-11-02 11:42:47 -0500

    Create nucleotide_to_protein.py

---
+++ nucleotide_to_protein.py
@@ -0,0 +1,12 @@
+# Author:   Kate Cooper, kmcooper [at] unomaha [dot] edu
+# Created:  November 2, 2021
+# Updated:  November 2, 2021
+# Purpose:  This is not a real working Python program, its just an example
+
+# Bring in necessary libraries
+import Bio
+import pandas
+import nltk
+
+# Implement DNA to protein conversion
+print("Hello world!")
